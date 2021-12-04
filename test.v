774d3c2c962ae461d7bc47b7471cb9b770c1f28c
Rawan Hamad <samehsrrj@aucegypt.edu>
2021-12-04 22:25:35 +0200

    init

--- test.v
+++ test.v
@@ -1,28 +1,95 @@
-module top
-  (
-   input CLK, 
-   input RST,
-   input enable,
-   input [31:0] value,
-   output [7:0] led
+module test8bit(D_IN, CLK, EN, D_OUT);
+  wire [7:0] _00_;
+  input CLK;
+  input [7:0] D_IN;
+  output [7:0] D_OUT;
+  input EN;
+  sky130_fd_sc_hd__mux2_1 _01_ (
+    .A0(D_OUT[5]),
+    .A1(D_IN[5]),
+    .S(EN),
+    .X(_00_[5])
   );
-  reg [31:0] count;
-  reg [7:0] state;
-  assign led = count[23:16];
-  always @(posedge CLK) begin
-    if(RST) begin
-      count <= 0;
-      state <= 0;
-    end else begin
-      if(state == 0) begin
-        if(enable) state <= 1;
-      end else if(state == 1) begin
-        state <= 2;
-      end else if(state == 2) begin
-        count <= count + value;
-        state <= 0;
-      end
-    end
-  end
+  sky130_fd_sc_hd__mux2_1 _02_ (
+    .A0(D_OUT[4]),
+    .A1(D_IN[4]),
+    .S(EN),
+    .X(_00_[4])
+  );
+  sky130_fd_sc_hd__mux2_1 _03_ (
+    .A0(D_OUT[0]),
+    .A1(D_IN[0]),
+    .S(EN),
+    .X(_00_[0])
+  );
+  sky130_fd_sc_hd__mux2_1 _04_ (
+    .A0(D_OUT[2]),
+    .A1(D_IN[2]),
+    .S(EN),
+    .X(_00_[2])
+  );
+  sky130_fd_sc_hd__mux2_1 _05_ (
+    .A0(D_OUT[1]),
+    .A1(D_IN[1]),
+    .S(EN),
+    .X(_00_[1])
+  );
+  sky130_fd_sc_hd__mux2_1 _06_ (
+    .A0(D_OUT[7]),
+    .A1(D_IN[7]),
+    .S(EN),
+    .X(_00_[7])
+  );
+  sky130_fd_sc_hd__mux2_1 _07_ (
+    .A0(D_OUT[3]),
+    .A1(D_IN[3]),
+    .S(EN),
+    .X(_00_[3])
+  );
+  sky130_fd_sc_hd__mux2_1 _08_ (
+    .A0(D_OUT[6]),
+    .A1(D_IN[6]),
+    .S(EN),
+    .X(_00_[6])
+  );
+  sky130_fd_sc_hd__dfxtp_1 _09_ (
+    .CLK(CLK),
+    .D(_00_[0]),
+    .Q(D_OUT[0])
+  );
+  sky130_fd_sc_hd__dfxtp_1 _10_ (
+    .CLK(CLK),
+    .D(_00_[1]),
+    .Q(D_OUT[1])
+  );
+  sky130_fd_sc_hd__dfxtp_1 _11_ (
+    .CLK(CLK),
+    .D(_00_[2]),
+    .Q(D_OUT[2])
+  );
+  sky130_fd_sc_hd__dfxtp_1 _12_ (
+    .CLK(CLK),
+    .D(_00_[3]),
+    .Q(D_OUT[3])
+  );
+  sky130_fd_sc_hd__dfxtp_1 _13_ (
+    .CLK(CLK),
+    .D(_00_[4]),
+    .Q(D_OUT[4])
+  );
+  sky130_fd_sc_hd__dfxtp_1 _14_ (
+    .CLK(CLK),
+    .D(_00_[5]),
+    .Q(D_OUT[5])
+  );
+  sky130_fd_sc_hd__dfxtp_1 _15_ (
+    .CLK(CLK),
+    .D(_00_[6]),
+    .Q(D_OUT[6])
+  );
+  sky130_fd_sc_hd__dfxtp_1 _16_ (
+    .CLK(CLK),
+    .D(_00_[7]),
+    .Q(D_OUT[7])
+  );
 endmodule
-
