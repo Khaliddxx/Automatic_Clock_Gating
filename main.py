0f0fde8b85d6931049a239b8d1fc5265c11367b6
Khaliddxx <doodz@aucegypt.edu>
2021-12-07 19:55:34 +0200

    yosys working, will add tests

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import sys
 if __name__ == "__main__":
 	rtl = sys.argv[1]
 	# abstract syntax tree
-	ast = parse([rtl])
+	ast,_ = parse([rtl])
 	# Pyverilog has [description] tree, get root
 	description = ast.description
 	
@@ -16,21 +16,60 @@ if __name__ == "__main__":
 	
 	# Instantiating clock gate components
 	newRTL = []
-	inputList = []
-	outputList = []
-	clkGateArgs = [
-	vast.PortArg("GCLK", vast.Identifier("__clockgate_output_gclk_")),
- 	vast.PortArg("GATE", vast.Identifier("EN")),
- 	vast.PortArg("CLK", vast.Identifier("CLK"))
-	]
+	enables = []
+	en = []
+	List = []
+	clkGateArgs = []
+    
 
 
-	clkgate_cell = vast.Instance(
-	"sky130_fd_sc_hd__dlclkp",
-	"__clockgate_cell__",
-	tuple(clkgateArgs),
-	tuple()
-	)
-	clockgate_output_gclk = vast.Wire('__clockgate_output_gclk_')
+	for itemDeclaration in definition.items:
+		item_type = type(itemDeclaration).__name__
+		if item_type == "InstanceList":
+			mux = itemDeclaration.instances[0]
+			if mux.module == "sky130_fd_sc_hd__a21oi_1":
+				for i in mux.portlist:
+					if i.portname == "A1":
+						en.append(i.argname)
+			if mux.module == "sky130_fd_sc_hd__mux2_1":
+				for i in mux.portlist:
+					if i.portname == "S":
+						en.append(i.argname)
+			for i in mux.portlist:
+				if i.portname == "CLK":
+					i.argname = vast.Identifier('_clockgate_output_')
+
+
+	enables = list(set(en))
+	#print(enables)
+	for j in range(len(enables)):
+		clkGateArgs.append([vast.PortArg("GCLK", vast.Identifier("_clockgate_output_")), vast.PortArg("GATE", vast.Identifier(str(en[j]))), vast.PortArg("CLK", vast.Identifier("CLK"))])
 	
-	
+    
+	clockgate_output_gclk = vast.Wire('_clockgate_output_')
+	newRTL.append(clockgate_output_gclk)
+    
+	for itemDeclaration in definition.items:
+		item_type = type(itemDeclaration).__name__
+		if item_type != "InstanceList":
+			newRTL.append(itemDeclaration)
+		else:
+			mux = itemDeclaration.instances[0]
+			if mux.module != "sky130_fd_sc_hd__mux2_1" and mux.module != "sky130_fd_sc_hd__a21oi_1":
+				newRTL.append(itemDeclaration)
+
+
+	for j in range(len(enables)):       
+		clkgate_cell = vast.Instance("sky130_fd_sc_hd__dlclkp_1", "__clockgate_cell__", tuple(clkGateArgs[j]), tuple())
+		newRTL.append(vast.InstanceList("sky130_fd_sc_hd__dlclkp_1", tuple(), tuple([clkgate_cell])))
+    
+	definition.items = tuple(newRTL)
+	codegen = ASTCodeGenerator()
+	rslt = codegen.visit(ast)
+	f = open("test1_out.v", "w+")
+	f.write(rslt)
+	f.close()
+
+
+
+
