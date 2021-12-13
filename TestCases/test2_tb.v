eed07b690075bca311004946054c3e951d36992a
Khaliddxx <doodz@aucegypt.edu>
2021-12-13 11:14:42 +0200

    Test cases added, unresolved ABC error on tests 3 & 4

--- TestCases/test2_tb.v
+++ TestCases/test2_tb.v
@@ -1,9 +1,9 @@
 module tb;
-reg CLK,EN;
+reg clk,en;
 reg [7:0] D_IN;
 wire [7:0] D_OUT;
 
-test2 x (D_IN,CLK,EN,D_OUT);
+test2 x (D_IN,clk,en,D_OUT);
 
 initial begin
 $dumpfile("test_updated.vcd"); // vcd dump file
@@ -11,16 +11,16 @@ $dumpvars; // dump everything
 end 
 
  initial begin 
-	CLK = 0;
+	clk = 0;
     forever begin
     #5
-    CLK = ~CLK;
+    clk = ~clk;
         end 
  end
 
 initial begin
 D_IN=7'd1;
-EN=1'b1;
+en=1'b1;
 #10
 if(D_OUT==7'b1)  
         $display("test correct");
@@ -29,7 +29,7 @@ else
 #10
 
 D_IN=7'b0;
-EN=1'b0;
+en=1'b0;
 #10
 if(D_OUT==7'b1)  
         $display("test correct");
@@ -38,7 +38,7 @@ else
 #10
 
 D_IN=7'b1;
-EN=1'b0;
+en=1'b0;
 #10
 if(D_OUT==7'b1)  
        $display("test correct");
@@ -47,7 +47,7 @@ else
 #10
 
 D_IN=7'b0;
-EN=1'b1;
+en=1'b1;
 #10
 if(D_OUT==7'b0)  
        $display("test correct");
@@ -55,7 +55,7 @@ else
         $display("test failed for input combination 01");
 #10 
 D_IN=7'd3;
-EN=1'b1;
+en=1'b1;
 #10
 if(D_OUT==7'd3)  
        $display("test correct");
@@ -63,7 +63,7 @@ else
         $display("test failed for input combination 01");
 #10
 D_IN=7'd13;
-EN=1'b0;
+en=1'b0;
 #10
 if(D_OUT==7'd3)  
        $display("test correct");
@@ -71,7 +71,7 @@ else
         $display("test failed for input combination 01");
 #10
 D_IN=7'd13;
-EN=1'b1;
+en=1'b1;
 #10
 if(D_OUT==7'd13)  
        $display("test correct");
@@ -79,14 +79,14 @@ else
         $display("test failed for input combination 01");
 #10
 D_IN=7'd13;
-EN=1'b1;
+en=1'b1;
 #10
 if(D_OUT==7'd13)  
        $display("test correct");
 else
         $display("test failed for input combination 01");#10
 D_IN=7'd254;
-EN=1'b1;
+en=1'b1;
 #10
 if(D_OUT==7'd254)  
        $display("test correct");
